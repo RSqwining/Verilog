f44175cf4620c3cf5299520f4cc8cb7551842578
RSqwining <147663623+RSqwining@users.noreply.github.com>
2023-12-29 19:33:24 -0800

    Update ThreeByOneMultiplexer_tb.v

--- MUX/ThreeByOneMultiplexer_tb.v
+++ MUX/ThreeByOneMultiplexer_tb.v
@@ -1,9 +1,7 @@
-```verilog
 module ThreeByOneMultiplexer_tb;
     reg A, B, C, sel1, sel2;
     wire D;
-    ThreeByOneMultiplexer il (.A (A), .B (B), .C (C), .sel1 (sel1), .sel2
-(sel2), .out (D));
+    ThreeByOneMultiplexer il (.A (A), .B (B), .C (C), .sel1 (sel1), .sel2(sel2), .out (D));
     initial
 begin
             // sel1 = 0, sel2 = 0
@@ -97,4 +95,3 @@ begin
             #20;
 end
 endmodule
-```
